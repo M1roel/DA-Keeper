9d5334b1ee0faca0dc374ee8c75c159740f8c923
M1roel <ppfautsch@yahoo.de>
2024-05-26 19:13:03 +0200

    Header erstellt

---
+++ script.js
@@ -0,0 +1,9 @@
+function renderHeader() {
+  let content = document.getElementById("header");
+  content.innerHTML = "";
+  content.innerHTML += `<h1>Keeper App</h1>`;
+}
+
+function initialize() {
+    renderHeader();
+  }
